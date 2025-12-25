b11a2ecca5d22e80e8867acf8f54c568dfdc37a4
burrellka <kmax18@hotmail.com>
2025-12-25 08:17:09 -0500

    savings progress before epsiode order change

---
+++ TestIntrospect/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Reflection;
+using MediaBrowser.Controller.Library;
+
+namespace TestIntrospect
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var type = typeof(IUserManager);
+            Console.WriteLine($"\nInspecting IUserManager: {type.FullName}");
+            
+            Console.WriteLine("\nProperties:");
+            foreach (var p in type.GetProperties())
+            {
+                 Console.WriteLine($" - {p.Name} ({p.PropertyType})");
+            }
+
+            Console.WriteLine("\nGetUserById Signature:");
+            var method = type.GetMethod("GetUserById");
+            if (method != null)
+            {
+                Console.WriteLine($" - Return: {method.ReturnType}");
+                foreach (var p in method.GetParameters())
+                {
+                    Console.WriteLine($" - Param: {p.Name} ({p.ParameterType})");
+                }
+            }
+        }
+    }
+}
